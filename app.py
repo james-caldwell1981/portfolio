40120eb8b87fd5118b06416396a86305f9c7e676
james-caldwell1981 <james.caldwell.1981@gmail.com>
2022-01-03 21:24:17 -0500

    Deleted pages.py for rewrite, added todo to app.py and removed unused import, added requirements.txt

--- app.py
+++ app.py
@@ -1,8 +1,5 @@
 from flask import Flask
 from flask import render_template
-from flask import url_for
-
-import pages
 
 
 app = Flask(__name__)
@@ -11,6 +8,7 @@ app = Flask(__name__)
 @app.route('/')
 def landing():  # put application's code here
 
+    #TODO Create page classes
     heading1 = 'Etiam finibus tempus felis eget ullamcorper'
     content1 = 'Cras pellentesque neque, consectetur adipiscing elit. Fusce venenatis elit ac ligula euismod varius. Maecenas feugiat purus quam, vel aliquet tellus sodales eu. Integer vitae vestibulum turpis, in sollicitudin quam. Aliquam vulputate non felis non luctus. Vestibulum eget augue nulla. Aliquam dignissim nibh mi, ut bibendum quam dapibus eget. Nam nec nisi vel sapien pulvinar ultrices. Curabitur rutrum nisl sagittis, pretium velit eget, convallis lacus. Nulla fermentum nulla ut lacus maximus, ut porttitor neque varius. Nullam sed vulputate sem. Sed bibendum porttitor metus.'
 
@@ -54,7 +52,6 @@ def landing():  # put application's code here
         ('Name 3', '#'),
         ('Name 4', '#'),
     ]
-
 
     rows = [[heading1, content1], [heading2, content2], [heading3, content3]]
     return render_template('page_intro.html',
