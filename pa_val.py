784cabc65d60a90612aadb77380d390537a5496e
james-caldwell1981 <james.caldwell.1981@gmail.com>
2022-01-07 02:09:42 -0500

    test auto-update

--- pa_val.py
+++ pa_val.py
@@ -5,6 +5,8 @@ import hashlib
 def is_valid_signature(x_hub_signature, data, private_key):
     # x_hub_signature and data are from the webhook payload
     # private key is your webhook secret
+    if x_hub_signature is None:
+        print(data)
     hash_algorithm, github_signature = x_hub_signature.split('=', 1)
     algorithm = hashlib.__dict__.get(hash_algorithm)
     encoded_key = bytes(private_key, 'latin-1')
