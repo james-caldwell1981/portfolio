acb43b90a362876108dfa62732012a8f035822d8
james-caldwell1981 <james.caldwell.1981@gmail.com>
2022-01-07 03:22:23 -0500

    test auto-update

--- flask_app.py
+++ flask_app.py
@@ -69,9 +69,8 @@ def landing():  # put application's code here
 @app.route('/update_server', methods=['POST'])
 def update_server():
     if request.method == 'POST':
-        x_hub_signature = request.headers('X-Hub-Signature-256')
-        print([i for i in request.data()])
-        private_key = environ['SECRET_KEY']
+        #x_hub_signature = request.headers.get('X-Hub-Signature-256')
+        #private_key = environ['SECRET_KEY']
         #if not is_valid_signature(x_hub_signature, request.data(), private_key):
         #    return 'Invalid signature.'
 
