1abe878ec200b42eeb17dd7de80c35670e24cd9f
james-caldwell1981 <james.caldwell.1981@gmail.com>
2022-01-14 22:42:26 -0500

    deleted pages branch to reassess workflow

---
+++ pages/temp_page_defs.py
@@ -0,0 +1,19 @@
+'''
+pg_id, pg_type, section, title,
+                 intro_text, images,
+                 rows=None
+'''
+page1 = {
+    'pg_id': 00000,
+    'pg_type': 'landing',
+    'section': 'home',
+    'title': 'landing',
+    'intro_text': {
+        'intro0': 'Welcome to the landing page',
+        'intro1': 'intro 1 section contains some stuff',
+        'intro2': 'intro 2 section contains some other stuff',
+        'intro3': 'intro 3 section has the last of the stuff'
+    }
+    'images': {},
+    'rows': {}
+}
