bb0d0fe4f280519bd3a8a6277a3e99a16c844691
james-caldwell1981 <james.caldwell.1981@gmail.com>
2022-01-17 19:43:37 -0500

    updated with docstrings and comments

--- flask_app.py
+++ flask_app.py
@@ -9,6 +9,8 @@ app = Flask(__name__)
 @app.route('/')
 def landing():  # put application's code here
 
+    # Currently using filler data for early testing
+
     heading1 = 'Etiam finibus tempus felis eget ullamcorper'
     content1 = 'Cras pellentesque neque, consectetur adipiscing elit. Fusce venenatis elit ac ligula euismod varius. Maecenas feugiat purus quam, vel aliquet tellus sodales eu. Integer vitae vestibulum turpis, in sollicitudin quam. Aliquam vulputate non felis non luctus. Vestibulum eget augue nulla. Aliquam dignissim nibh mi, ut bibendum quam dapibus eget. Nam nec nisi vel sapien pulvinar ultrices. Curabitur rutrum nisl sagittis, pretium velit eget, convallis lacus. Nulla fermentum nulla ut lacus maximus, ut porttitor neque varius. Nullam sed vulputate sem. Sed bibendum porttitor metus.'
 
@@ -54,6 +56,10 @@ def landing():  # put application's code here
     ]
 
     rows = [[heading1, content1], [heading2, content2], [heading3, content3]]
+
+    # This is currently how the page is populated
+    # Individual sections are a list of most recent items
+    # This should be updated in the future for readability and modularity
     return render_template('page_intro.html',
                            title='Viral',
                            articles=articles,
@@ -65,14 +71,31 @@ def landing():  # put application's code here
                            custom_layout=None,
                            custom_script=None)
 
+
 @app.route('/update_server', methods=['POST'])
 def update_server():
-    if request.method == 'POST':
-        x_hub_signature = request.headers.get('X-Hub-Signature-256')
-        private_key = environ['SECRET_KEY']
-        if not is_valid_signature(x_hub_signature, request.data, private_key):
-            return 'Invalid signature.'
 
+    """
+    This route receives a call from the github webhook detecting a new merge.
+    After a successful merge, the webhook sends a push request to the deployed
+    application which then attempts to verify identity. Upon success,
+    the GitHub hook is allowed to push the new data to the deployed repo.
+
+    :return: Plain text disposition and status code.
+    """
+
+    if request.method == 'POST':
+        # Gets the x-hub-signature from the request header
+        x_hub_signature = request.headers.get('X-Hub-Signature-256')
+
+        # Load the secret from environment
+        private_key = environ['SECRET_KEY']
+
+        # If verification fails, return plaintext error with code
+        if not is_valid_signature(x_hub_signature, request.data, private_key):
+            return 'Invalid signature.', 401
+
+        # Set up the repository and pull it's contents
         repo = Repo('/home/jamescaldwell1981/portfolio')
         origin = repo.remotes.origin
         origin.pull()
