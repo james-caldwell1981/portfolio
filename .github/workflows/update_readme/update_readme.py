d87fc6613e8cfb469969a1dcf89fec5ec48c63ce
james-caldwell1981 <james.caldwell.1981@gmail.com>
2022-02-21 20:28:22 -0500

    reworking directory structure in get content function

--- .github/workflows/update_readme/update_readme.py
+++ .github/workflows/update_readme/update_readme.py
@@ -3,15 +3,15 @@ from jinja2 import Template
 
 
 repo_directories = (
-    'static'
+    'static',
 )
 
 def get_content(repo_directories):
     return_content = {}
 
     for directory in repo_directories:
-        new_content = Path('../../../' + directory + '/' + directory + '_readme.md').read_text()
-        return_content[directory] = (directory[0].upper() + directory[1:], new_content)
+        new_content = Path(f'{directory}_readme.md').read_text()
+        return_content[directory] = (directory, new_content)
 
     return return_content
 
@@ -23,6 +23,6 @@ template = Template(Path(main_readme_template_path).read_text())
 Path(readme_path).write_text(
         template.render(
             table_of_contents='Testing',
-            body='1, 2, 3',
+            body=get_content(['.github/workflows']),
             )
         )
