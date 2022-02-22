541a99b74cff45872de2fc64a6948166bf25bdae
james-caldwell1981 <66021725+james-caldwell1981@users.noreply.github.com>
2022-02-21 19:43:43 -0500

    Update update_readme.py

--- .github/workflows/update_readme/update_readme.py
+++ .github/workflows/update_readme/update_readme.py
@@ -22,6 +22,6 @@ template = Template(Path(main_readme_template_path).read_text())
 Path(main_readme_template_path).write_text(
         template.render(
             table_of_contents='Testing',
-            body=get_content(['.github/workflows']),
+            body=get_content(['./']),
             )
         )
